9cc5386682b76623252a7910aa8e3e43bd0070c0
wanghongxin492 <wanghongxin492@emao.com>
2016-12-01 20:23:02 +0800

    提交栅格化

--- src/modules/components/MainSection/MainSection.jsx
+++ src/modules/components/MainSection/MainSection.jsx
@@ -1,87 +1,63 @@
-import React, { Component, PropTypes } from 'react'
-import TodoItem from '../TodoItem/TodoItem'
-import Footer from '../Footer/Footer'
-import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../../constants/TodoFilters'
-
-
-const TODO_FILTERS = {
-    [SHOW_ALL]: () => true,
-    [SHOW_ACTIVE]: todo => !todo.completed,
-    [SHOW_COMPLETED]: todo => todo.completed
-}
+import React,{ Component } from 'react'
 
 class MainSection extends Component {
-    constructor(props, context) {
-        super(props, context)
-        this.state = { filter: SHOW_ALL }
-    }
-
-    handleClearCompleted() {
-        const atLeastOneCompleted = this.props.todos.some(todo => todo.completed)
-        if (atLeastOneCompleted) {
-            this.props.actions.clearCompleted()
-        }
-    }
-
-    handleShow(filter) {
-        this.setState({ filter })
-    }
-
-    renderToggleAll(completedCount) {
-        const { todos, actions } = this.props
-        if (todos.length > 0) {
-            return (
-                <input className="toggle-all"
-                    type="checkbox"
-                    checked={completedCount === todos.length}
-                    onChange={actions.completeAll} />
-            )
-        }
-    }
-
-    renderFooter(completedCount) {
-        const { todos } = this.props
-        const { filter } = this.state
-        const activeCount = todos.length - completedCount
-
-        if (todos.length) {
-            return (
-                <Footer completedCount={completedCount}
-                    activeCount={activeCount}
-                    filter={filter}
-                    onClearCompleted={this.handleClearCompleted.bind(this) }
-                    onShow={this.handleShow.bind(this) } />
-            )
-        }
-    }
-
     render() {
-        const { todos, actions } = this.props
-        const { filter } = this.state
-
-        const filteredTodos = todos.filter(TODO_FILTERS[filter])
-        const completedCount = todos.reduce((count, todo) =>
-            todo.completed ? count + 1 : count,
-            0
-        )
-
         return (
             <section className="main">
-                {this.renderToggleAll(completedCount) }
-                <ul className="todo-list">
-                  {filteredTodos.map(todo =>
-                      <TodoItem key={todo.id} todo={todo} {...actions} />
-                  ) }
-                    </ul>
-                {this.renderFooter(completedCount) }
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="number">12</div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-3">
+                            <div className="number">3</div>
+                        </div>
+                        <div className="col-3">
+                            <div className="number">3</div>
+                        </div>
+                        <div className="col-3">
+                            <div className="number">3</div>
+                        </div>
+                        <div className="col-3">
+                            <div className="number">3</div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-4">
+                            <div className="number">4</div>
+                        </div>
+                        <div className="col-4">
+                            <div className="number">4</div>
+                        </div>
+                        <div className="col-4">
+                            <div className="number">4</div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-6">
+                            <div className="number">6</div>
+                        </div>
+                        <div className="col-4">
+                            <div className="number">4</div>
+                        </div>
+                        <div className="col-2">
+                            <div className="number">2</div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-2">
+                            <div className="number">2</div>
+                        </div>
+                        <div className="col-10">
+                            <div className="number">10</div>
+                        </div>
+                    </div>
+                </div>
             </section>
         )
     }
-}
-
-MainSection.propTypes = {
-    todos: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
 }
 
 export default MainSection
